3eb8f59d27a93d32c6a698df80a442d0e72abb47
SimonSimonson <simon@keppgrund5.de>
2019-11-07 21:41:22 +0100

    TUI

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -2,6 +2,8 @@ class Game () {
 
   def start(): Unit= {
     val map = new Map
+
+
     val karte = new Card(0, 2, 1, 3)
     karte.print()
     karte.rotate()
--- src/main/scala/Map.scala
+++ src/main/scala/Map.scala
@@ -1,1 +1,12 @@
-case class Map()
+case class Map(x: Int, y: Int){
+  val field = Array.ofDim[Card](x,y)
+  val mid = (x/2,y/2)
+  def Setcard(card : Card, x:Int , y : Int): Int ={
+    if(field(x)(y) != null)
+      return -1
+    field(x)(y) = card
+    return 1
+  }
+
+
+}
