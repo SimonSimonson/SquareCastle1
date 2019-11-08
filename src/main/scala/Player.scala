92f9776a72f77ffdc8ac210e83307879353b48e4
SimonSimonson <simon@keppgrund5.de>
2019-11-08 08:56:10 +0100

    verbesserte Methoden

--- src/main/scala/Player.scala
+++ src/main/scala/Player.scala
@@ -5,5 +5,4 @@ import scala.collection.mutable.ListBuffer
 case class Player(name: String ) {
     val karten = new ListBuffer[Int]()
 
-
 }
--- src/test/scala/PlayerTest.scala
+++ src/test/scala/PlayerTest.scala
@@ -1,16 +1,16 @@
-/*import org.scalatest._
+import org.scalatest._
 
 
 class PlayerTest extends WordSpec with Matchers {
 
   "A player" when { "new" should {
-    val player = Player("Name")
+    val player = Player("Julian")
     "have a name"  in {
-      player.value should be("Julian")
+      player.name should be("Julian")
     }
     "have a nice String representation" in {
       player.toString should be("Julian")
     }
   }}
 
-}*/
+}
