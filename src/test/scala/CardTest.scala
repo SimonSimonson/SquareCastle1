fe312ce867cbfe3f39ed7719dff5ce945627cb4f
juelsen <juelsen98@gmx.de>
2019-11-08 09:36:21 +0100

    commit CardTest

--- src/test/scala/CardTest.scala
+++ src/test/scala/CardTest.scala
@@ -7,11 +7,29 @@ class CardTest extends WordSpec with Matchers {
   "A Card" when { "new" should {
     var cards = Card(0,1,2,3)
     "have an Array"  in {
-      cards.side3 should be (3)
+      cards.side3 should be (2)
     }
     "when rotate"  in {
       cards.rotate() should be (true)
       cards.mysides(3) should be (2)
+    }
+    "when karte passt" in {
+      val karte1 = new Card(0,2,1,2)
+      val karte2 = new Card(0,1,2,2)
+      karte1.anlegen(1, karte2) should be (true)
+
+    }
+    "when karte passt nicht" in {
+      val karte1 = new Card(0,2,1,2)
+      val karte2 = new Card(0,1,2,1)
+      karte1.anlegen(1, karte2) should be (false)
+
+    }
+    "when alone" in {
+      cards.CheckifAlone() should be (true)
+      cards.none should be (null)
+      cards.roads should be (null)
+      cards.castle should be (null)
     }
 
   }}
---
+++ src/test/scala/MapTest.scala
@@ -0,0 +1,16 @@
+
+import org.scalatest._
+
+class MapTest extends WordSpec with Matchers {
+
+  "A player" when { "new" should {
+    val player = Player("Julian")
+    "have a name"  in {
+      player.name should be("Julian")
+    }
+    "have a nice String representation" in {
+      player.toString should be("Player(Julian)")
+    }
+  }}
+
+}
