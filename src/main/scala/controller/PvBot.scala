7fccc12475b0e198e674fef232ec0a7afd69dd0b
juelsen <juelsen98@gmx.de>
2019-12-06 11:45:52 +0100

    commit: Commands

--- src/main/scala/controller/PvBot.scala
+++ src/main/scala/controller/PvBot.scala
@@ -7,16 +7,16 @@ import util.State
 
 case class StateB() extends State(){
 
-  override def handle(state:Boolean,controller:Controller, playerA:Player, playerB:Player, bot:KI, map:Map, card: Card): Int ={
-    if(state) {
+  override def handle(state:Boolean,controller:Controller, playerA:Player, playerB:Player, bot:KI, map:Map, card: Card): Int = {
+    if (state) {
       return controller.Optionen(card, map, playerA) //kann erstmal nur einen Player
-
     }
     else
-      controller.Calculatebot(bot,card,map)
+      controller.Calculatebot(bot, card, map)
     1
+  }
 
-  override def state(): Int={
+  override def state(): Int ={
     return 1
   }
 
